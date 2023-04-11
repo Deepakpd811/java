3d8a159dad6d20f0587db9ac978a15926da1ff9f
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2023-04-11 23:56:21 +0530

    add bubblesort

---
+++ bubblesort.java
@@ -0,0 +1,22 @@
+public class bubblesort {
+    public static void main(String[] args) {
+
+        int[] arr = { 7, 3, 9, 2, 1, 4 };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
